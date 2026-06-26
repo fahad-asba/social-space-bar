9aa7fa9334a25d293a2558f5e323a35dbcfa4f14
Anas Ismail <100934197+AnasIsmail8996@users.noreply.github.com>
2026-06-26 22:02:18 +0500

    new email added new styles

--- src/lib/phone.ts
+++ src/lib/phone.ts
@@ -1,5 +1,5 @@
-export const PHONE_DISPLAY = '(210) 493-8277';
-export const PHONE_HREF = 'tel:+12104938277';
+export const PHONE_DISPLAY = '+1 (210) 493-8277';
+export const PHONE_HREF = 'tel:+1-210-493-8277';
 export const PHONE_ARIA_LABEL = `Call ${PHONE_DISPLAY}`;
 export const PHONE_SCHEMA = '+1-210-493-8277';
 
